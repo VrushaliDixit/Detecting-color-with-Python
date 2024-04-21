22c59667fbf3a2f49b42d34d6b7e4256260ca7dc
VrushaliDixit <89120519+VrushaliDixit@users.noreply.github.com>
2024-04-21 20:18:21 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,51 @@
-import cv2
-from PIL import Image
+import os
+import pickle
 
-from util import get_limits
+from skimage.io import imread
+from skimage.transform import resize
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 
-yellow = [0, 255, 255]  # yellow in BGR colorspace
-cap = cv2.VideoCapture(2)
-while True:
-    ret, frame = cap.read()
+# prepare data
+input_dir = '/home/phillip/Desktop/todays_tutorial/19_parking_car_counter/code/clf-data'
+categories = ['empty', 'not_empty']
 
-    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+data = []
+labels = []
+for category_idx, category in enumerate(categories):
+    for file in os.listdir(os.path.join(input_dir, category)):
+        img_path = os.path.join(input_dir, category, file)
+        img = imread(img_path)
+        img = resize(img, (15, 15))
+        data.append(img.flatten())
+        labels.append(category_idx)
 
-    lowerLimit, upperLimit = get_limits(color=yellow)
+data = np.asarray(data)
+labels = np.asarray(labels)
 
-    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+# train / test split
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
-    mask_ = Image.fromarray(mask)
+# train classifier
+classifier = SVC()
 
-    bbox = mask_.getbbox()
+parameters = [{'gamma': [0.01, 0.001, 0.0001], 'C': [1, 10, 100, 1000]}]
 
-    if bbox is not None:
-        x1, y1, x2, y2 = bbox
+grid_search = GridSearchCV(classifier, parameters)
 
-        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+grid_search.fit(x_train, y_train)
 
-    cv2.imshow('frame', frame)
+# test performance
+best_estimator = grid_search.best_estimator_
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+y_prediction = best_estimator.predict(x_test)
 
-cap.release()
+score = accuracy_score(y_prediction, y_test)
 
-cv2.destroyAllWindows()
+print('{}% of samples were correctly classified'.format(str(score * 100)))
 
+pickle.dump(best_estimator, open('./model.p', 'wb'))
